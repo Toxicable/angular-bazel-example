fb6ccafa31e3781d32d589a2eda29e422a719309
Fabian Wiles <fabianwiles@live.com>
2018-02-17 14:22:13 +1300

    moreeee size

--- src/hello-world/hello-world.component.ts
+++ src/hello-world/hello-world.component.ts
@@ -1,18 +1,39 @@
 import {Component, NgModule} from '@angular/core';
 import {msg} from '../lib/file';
-
+// some comment
 @Component({
   selector: 'hello-world-app',
   template: `
     <div>Hello {{ name }}</div>
     <input type="text" [value]="name" (input)="name = $event.target.value"/>
     hhheellllooo
-    1234567890
-    1234567890
-    1234567890
-    1234567890
-    1234567890
-    1234567890
+    hhheellllooo
+    hhheellllooo
+    hhheellllooo
+    hhheellllooo
+    hhheellllooo
+    hhheellllooo
+    hhheellllooo
+    hhheellllooo
+    hhheellllooo
+    hhheellllooo
+    hhheellllooo
+    hhheellllooo    hhheellllooo
+    hhheellllooo
+    hhheellllooo
+    hhheellllooo
+    hhheellllooo
+    hhheellllooo
+    hhheellllooo
+    hhheellllooo
+    hhheellllooo
+    hhheellllooo
+    hhheellllooo
+    hhheellllooo
+    hhheellllooo
+    hhheellllooo
+    hhheellllooo
+    hhheellllooo
   `,
   styleUrls: ['./hello-world-styles.css']
 })
